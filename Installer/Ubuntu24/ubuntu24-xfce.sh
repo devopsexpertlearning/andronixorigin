a9d40f57303c3db49c750140285c6d250a7eec37
Ravi Kushwaha <98015488+devopsexpertlearning@users.noreply.github.com>
2025-03-18 08:47:52 +0000

    Update ubuntu24-xfce.sh

--- Installer/Ubuntu24/ubuntu24-xfce.sh
+++ Installer/Ubuntu24/ubuntu24-xfce.sh
@@ -235,6 +235,7 @@ echo "Installing XFCE Desktop .."
 
 bash $bin <<EOM
 apt update -y
+apt install systemctl -y
 apt install nano apt-utils -y
 apt install software-properties-common -y
 add-apt-repository ppa:mozillateam/ppa
@@ -244,10 +245,9 @@ Pin: release o=Ubuntu*
 Pin-Priority: -1' | tee /etc/apt/preferences.d/mozilla-firefox
 apt install firefox -y
 apt install libpciaccess0 libegl1-mesa -y
-apt install tigervnc-standalone-server tigervnc-common tigervnc-tools -y --no-install-recommends
-apt install xfce4 xfce4-goodies dbus-x11 -y --no-install-recommends
-apt install ca-certificates libcurl4 libgbm1 libnspr4 libnss3 xdg-utils -y --no-install-recommends
-apt install systemctl -y
+apt install tigervnc-standalone-server tigervnc-common tigervnc-tools -y
+apt install xfce4 xfce4-goodies dbus-x11 -y
+apt install ca-certificates libcurl4 libgbm1 libnspr4 libnss3 xdg-utils -y
 echo "Ubuntu 24 with XFCE setup is complete!"
 exit
 EOM
