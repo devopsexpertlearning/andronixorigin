dc4f9412d470bebab5298ff91ee3a0415b0b0943
Ravi Kushwaha <98015488+devopsexpertlearning@users.noreply.github.com>
2025-03-18 08:33:56 +0000

    Update ubuntu24-xfce.sh

--- Installer/Ubuntu24/ubuntu24-xfce.sh
+++ Installer/Ubuntu24/ubuntu24-xfce.sh
@@ -229,19 +229,27 @@ chmod +x $bin
 echo "removing image for some space"
 rm $tarball
 
-echo "#!/bin/bash"
+# Start Ubuntu and install additional packages
+echo "Installing required packages inside Ubuntu..."
+
+bash $bin <<EOM
 apt update -y
 apt install nano apt-utils -y
 apt install software-properties-common -y
 add-apt-repository ppa:mozillateam/ppa
 apt update -y
-echo 'Package: firefox*  
-Pin: release o=Ubuntu*  
+echo 'Package: firefox*  
+Pin: release o=Ubuntu*  
 Pin-Priority: -1' | tee /etc/apt/preferences.d/mozilla-firefox
 apt install firefox -y
 apt install libpciaccess0 libegl1-mesa -y
-apt install tigervnc-standalone-server tigervnc-common --no-install-recommends tigervnc-tools -y
-apt install xfce4 xfce4-goodies dbus-x11 --no-install-recommends -y
-apt install ca-certificates libcurl4 libgbm1 libnspr4 libnss3 xdg-utils --no-install-recommends -y
-echo "You can now launch Ubuntu with the ./${bin} script from next time"
+apt install tigervnc-standalone-server tigervnc-common tigervnc-tools -y --no-install-recommends
+apt install xfce4 xfce4-goodies dbus-x11 -y --no-install-recommends
+apt install ca-certificates libcurl4 libgbm1 libnspr4 libnss3 xdg-utils -y --no-install-recommends
+
+echo "Ubuntu 24 with XFCE setup is complete!"
+exit
+EOM
+
+echo "You can now launch Ubuntu with ./${bin}"
 bash $bin
